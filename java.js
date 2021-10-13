2b8823ae981cbccf71d6e2523033880a861520cb
pmarx92 <pamarx92@gmail.com>
2021-10-13 15:39:37 +0200

    added html to link .js, added a function to the .js

---
+++ java.js
@@ -0,0 +1,8 @@
+var items = ['Rock', 'Paper', 'Scissors'];
+var item = items[Math.floor(Math.random() * items.length)];
+
+function computerPlay(item) {   
+    return item;
+
+}
+console.log(computerPlay);
